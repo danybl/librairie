cfd808deafebeed4be519b7a3a6aee2ede107e15
David Gallego <davidgallego0125@gmail.com>
2014-11-10 21:29:04 -0500

    miiiiiiawwwwwwwwwww

--- librairie/src/controllers/Connexion.java
+++ librairie/src/controllers/Connexion.java
@@ -19,7 +19,7 @@ public class Connexion extends HttpServlet {
 
     private static final String VUE_CONNEXION_FAIL = "/WEB-INF/jsp/connexion.jsp";
 
-    private static final String VUE_CONNEXION_SUCCESS = "livreSpecialite";
+    private static final String VUE_CONNEXION_SUCCESS = "/livreSpecialite";
 
     @Override
     public void doGet(HttpServletRequest request,
